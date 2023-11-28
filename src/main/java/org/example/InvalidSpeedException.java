83d4ea76f803c3ececfa5996c2fbc2066fea4093
nologyrob <rob.hooper@nology.io>
2023-11-28 15:06:58 +0000

    added exception class details

--- src/main/java/org/example/InvalidSpeedException.java
+++ src/main/java/org/example/InvalidSpeedException.java
@@ -2,3 +2,11 @@ package org.example;
 
 public class InvalidSpeedException {
 }
+
+
+//         Question 4: Exception Handling - Create your own
+//
+//        Extend the above code by adding functionality where:
+//
+//        - Setting a negative speed for any vehicle throws a custom exception named InvalidSpeedException.
+//        - The main function should catch this exception and print a user-friendly message without terminating.
